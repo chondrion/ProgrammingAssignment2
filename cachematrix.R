1f3e6cbb8aa59a4ee5f2f335b6844fc43a1ff8d3
chondrion <chondrion@yahoo.com>
2015-03-22 14:20:43 -0500

    update cachematrix.R
    Added code to cache Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions take and cache a matrix, then give the inverse of the cache matrix
 
-## Write a short comment describing this function
+## This function caches an inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        cachem <- x  ##Set value in makeCacheMatrix function
+        cacheinv <- solve(cachem)
+        getfunction <- function() cacheinv  ##return value
+        setfunction <- function(y = NULL) {       ##puts value in global
+                ##environment        
+                cacheinv <<- y 
+        }
+        
 }
 
 
-## Write a short comment describing this function
+## This function returns an inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        cacheinv <- cachem$getfunction()  ##gets cache matrix
+        
+        solve(x) ##solves for inverse of matrix 
+        cache$setfunction(x)  ##puts value in global environment
+        print(cache$getfunction()) ##prints the inverse of the matrix
 }
